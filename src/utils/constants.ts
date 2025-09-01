a89b9d442d83054f8d9bbff788dc608bb96dd7d6
Ankur Mursalin <mir.ankur,ruet13@gmail.com>
2025-09-01 17:24:46 +0600

    feat: add query support

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,14 +6,40 @@
  * Created on Sun Aug 31 2025
  */
 
-export const basePublicPath = process.env.NODE_ENV === 'production' ? '/markdown-to-slide' : ''
+const isProduction = process.env.NODE_ENV === 'production'
+
+export const basePublicPath = isProduction ? '/markdown-to-slide' : '';
+
+/**
+ * The domain on which the project will load
+ */
+export const baseDomain = isProduction ? 'https://encryptioner.github.io' : 'http://localhost:4000';
+export const origin = typeof window !== 'undefined' ? window.location.origin : baseDomain;
+
+export const AI_CHAT_BASE_CONFIG = {
+  domain: encodeURIComponent(`${origin}${basePublicPath}`),
+  systemMessage: encodeURIComponent(`
+    You are a helpful assistant.
+    Keep responses as concise as possible.
+    Avoid long explanations.
+    You must reply as poem while giving the answer.
+  `).replace(/\n/g, ' ')
+  }
+
+/**
+ * NOTE:
+ * Set to empty string to disable chat functionality
+ * For development: 'http://localhost:5173/embed.js'
+ * For production: 'https://username.github.io/in-browser-llm-inference/embed.js'
+ */
+const embedScriptMainSrc = isProduction 
+? 'https://encryptioner.github.io/in-browser-llm-inference/embed.js' 
+: 'http://localhost:5173/embed.js';
+
 
 // AI Chat Plugin Configuration
 export const AI_CHAT_CONFIG = {
-  // Set to empty string to disable chat functionality
-  // For development: 'http://localhost:5173/embed.js'
-  // For production: 'https://username.github.io/in-browser-llm-inference/embed.js'
-  embedScriptUrl: process.env.NODE_ENV === 'production' ? 'https://encryptioner.github.io/in-browser-llm-inference/embed.js' : 'http://localhost:5173/embed.js',
+  embedScriptUrl: `${embedScriptMainSrc}?system=${AI_CHAT_BASE_CONFIG.systemMessage}&domain=${AI_CHAT_BASE_CONFIG.domain}`,
   
   // Chat widget configuration
   enabled: true, // Set to false to completely disable chat widget
