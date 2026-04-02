93d080bd7db21518b424647d968b3d6f06168d4b
Ankur Mursalin <mir.ankur.ruet13@gmail.com>
2026-04-03 00:27:21 +0600

    refactor: redesign footer layout with balanced 3-column structure
    Remove QR code image, restructure footer to responsive 3-column layout, add support link separator, reduce padding for cleaner appearance

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,15 +4,15 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-800 text-white py-4 lg:py-6 mt-auto">
+    <footer className="bg-gray-800 text-white py-3 lg:py-4 mt-auto">
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-2 md:mb-0">
             <p className="text-sm">
               Created by{' '}
-              <a 
-                href="https://encryptioner.github.io/" 
-                target="_blank" 
+              <a
+                href="https://encryptioner.github.io/"
+                target="_blank"
                 rel="noopener noreferrer"
                 className="text-blue-400 hover:text-blue-300 cursor-pointer"
               >
@@ -20,7 +20,7 @@ const Footer: React.FC = () => {
               </a>
             </p>
           </div>
-          
+
           <div className="flex items-center space-x-4 mb-2 md:mb-0">
             <a
               href="https://github.com/Encryptioner/markdown-to-slide"
@@ -33,7 +33,7 @@ const Footer: React.FC = () => {
                 <path fillRule="evenodd" d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z" clipRule="evenodd" />
               </svg>
             </a>
-            
+
             <a
               href="https://linkedin.com/in/ankur-mursalin"
               target="_blank"
@@ -45,7 +45,7 @@ const Footer: React.FC = () => {
                 <path fillRule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clipRule="evenodd" />
               </svg>
             </a>
-            
+
             <a
               href="https://twitter.com/encryptioner"
               target="_blank"
@@ -57,7 +57,7 @@ const Footer: React.FC = () => {
                 <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
               </svg>
             </a>
-            
+
             <a
               href="https://encryptioner.github.io/blog"
               target="_blank"
@@ -70,10 +70,22 @@ const Footer: React.FC = () => {
                 <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V9a1 1 0 00-1-1h-1v3a2 2 0 01-2 2H5.828a1.828 1.828 0 01-1.414-.586l-1.828-1.828A1 1 0 013 9.172V7h1a2 2 0 012-2h8a2 2 0 012 2z" />
               </svg>
             </a>
+
+            <span className="text-gray-500">|</span>
+
+            <a
+              href="https://www.supportkori.com/mirmursalinankur"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-amber-300 hover:text-amber-200 transition-colors cursor-pointer text-sm font-medium"
+              title="Support"
+            >
+              ☕ Support
+            </a>
           </div>
-          
-          <div className="text-sm text-gray-300">
-            <p>Free & Open Source</p>
+
+          <div>
+            <span className="text-xs text-gray-400">Free &amp; open source</span>
           </div>
         </div>
         
